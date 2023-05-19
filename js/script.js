5e8e200ef9d96963bc18f7bf2cd0e956bab1742c
alex__kis <fenaethylaamiinloever@gmail.com>
2023-05-19 12:14:04 +0300

    burger button updated

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let burger = document.querySelector('.burger');
 let headerMenu = document.querySelector('.header__menu');
 burger.addEventListener('click', function () {
 	headerMenu.classList.toggle('header__menu--open');
-	burger.classList.toggle('burger--active')
+	burger.classList.toggle('burger--active');
 });
 
 let headerLinks = document.querySelectorAll('.header__menu a');
@@ -10,5 +10,6 @@ let headerLinks = document.querySelectorAll('.header__menu a');
 for (let headerLink of headerLinks) {
 	headerLink.addEventListener('click', function () {
 		headerMenu.classList.remove('header__menu--open');
+		burger.classList.toggle('burger--active');
 	});
 }
