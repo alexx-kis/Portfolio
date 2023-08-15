a86d9d8fea5006d3735535625679430a09bcf496
Alexander <fenaethylaamiinloever@gmail.com>
2023-08-15 21:57:55 +0300

    +scrollify

--- js/script.js
+++ js/script.js
@@ -75,3 +75,24 @@ window.addEventListener('load', function () {
 
 	}
 });
+
+
+/*==================================== SCROLLIFY ====================================*/
+
+$.scrollify({
+	section: ".scroll", 					//- класс секций, которые будут перелистываться
+	interstitialSection: "",
+	easing: "easeOutExpo",
+	scrollSpeed: 1100,
+	offset: 0,
+	scrollbars: true,
+	// standardScrollElements: ".contact",	//- секция, у которой будет обычный скролл
+	setHeights: true,					//- подстраивание высот секций под высоту экрана
+	overflowScroll: true,
+	updateHash: true,
+	touchScroll: true,
+	before: function () { },
+	after: function () { },
+	afterResize: function () { },
+	afterRender: function () { }
+});
